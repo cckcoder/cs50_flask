1a48603c4cf5aa3132194b838082fb74b6a15ebe
codewiz <c.chaiwat.ch@gmail.com>
2020-07-15 09:36:40 +0000

    Complete template
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 #! /use/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Hello, world"
-
-@app.route("/<string:name>")
-def hello(name):
-  name = name.capitalize()
-  return f"<h1>Hello, {name}</h1>"
+  return render_template("index.html")
